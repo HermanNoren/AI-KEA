95f24ef60218b589617a3bf5fa2baa37b04b3f17
KingHermando <herman.noren@outlook.com>
2024-10-02 10:47:06 +0200

    comment out question presets

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -63,8 +63,8 @@ export function ChatPanel({
         isAtBottom={isAtBottom}
         scrollToBottom={scrollToBottom}
       />
-
       <div className="mx-auto sm:max-w-2xl sm:px-4">
+        {/*
         <div className="mb-4 grid grid-cols-2 gap-2 px-4 sm:px-0">
           {messages.length === 0 &&
             exampleMessages.map((example, index) => (
@@ -99,7 +99,7 @@ export function ChatPanel({
               </div>
             ))}
         </div>
-
+*/}
         {messages?.length >= 2 ? (
           <div className="flex h-12 items-center justify-center">
             <div className="flex space-x-2">
