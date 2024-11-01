a388e0fc716013c0b7241fa4d14642b1803b197b
Marten-Granath <115485279+Marten-Granath@users.noreply.github.com>
2024-11-01 15:58:45 +0100

    Small update to notice about text generation

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -19,13 +19,15 @@ export function EmptyScreen() {
           Digitalization and AI in practice given by Chalmers University of
           Technology.
         </p>
+      </div>
+      <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
         <h1 className="text-lg font-semibold">OBS!</h1>
         <p className="leading-normal text-muted-foreground">
           To avoid paid subscriptions in creating this project, we are using
           free versions of various services. This comes with limitations to
-          the text size that can be generated. If you bypass this limit an
-          error will occur, after which you will have to refresh to continue
-          using our product.
+          the text size that can be generated per query. If you bypass this 
+          limit an error will occur, after which you will have to refresh to 
+          continue using our product.
         </p>
       </div>
     </div>
